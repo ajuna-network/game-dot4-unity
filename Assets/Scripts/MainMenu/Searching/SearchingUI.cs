9fa2125278109760b23b4cf0ec47cf8c419d38fa
Cedric Decoster <cedric.decoster@gmail.com>
2022-06-25 16:04:02 +0200

    adding queue state

--- Assets/Scripts/MainMenu/Searching/SearchingUI.cs
+++ Assets/Scripts/MainMenu/Searching/SearchingUI.cs
@@ -1,5 +1,9 @@
+using Ajuna.NetApi.Model.AjunaCommon;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,14 +16,93 @@ namespace MainMenu.Searching.UI
         public Button cancelBtn;
         public TextMeshProUGUI searchingText;
 
+        public NetworkManager Network => NetworkManager.Instance;
+
+        public enum SearchState { 
+            None,
+            MatchFound,
+            Canceled,
+            NeedToQueue,
+            WaitForPlayers,
+            CheckRunnerState,
+            Extrinsics
+        }
 
         IEnumerator Searching()
         {
+            var searchState = SearchState.None;
+
             searchingText.text = "searching for a game";
-            yield return new WaitForSeconds(2);
-            // await Task.Delay(TimeSpan.FromSeconds(2));
+
+            while(searchState != SearchState.MatchFound || searchState != SearchState.Canceled)
+            {
+                switch (searchState)
+                {
+                    case SearchState.NeedToQueue:
+                        searchingText.text = "waiting for queue";
+                        break;
+
+                    case SearchState.WaitForPlayers:
+                        searchingText.text = "waiting for players";
+                        break;
+
+                    case SearchState.CheckRunnerState:
+                        searchingText.text = "waiting for runner";
+                        break;
+
+                    case SearchState.Extrinsics:
+                        searchingText.text = "waiting for transaction";
+                        break;
+
+                    case SearchState.None:
+                    case SearchState.MatchFound:
+                    case SearchState.Canceled:
+                    default:
+                        break;
+                }
+
+                var searchStateTask = GetSearchState();
+                while (!searchStateTask.IsCompleted)
+                {
+                    yield return new WaitForSeconds(1);
+                }
+                searchState = searchStateTask.Result;
+            }
 
             StartCoroutine(nameof(MatchFound));
+        }
+
+        public async Task<SearchState> GetSearchState()
+        {
+            if (Network.Dot4GClient.HasExtrinsics > 0)
+            {
+                return SearchState.Extrinsics;
+            }
+
+            var playerQueued = await Network.Dot4GClient.GetPlayerQueueAsync();
+            
+            var runnerId = await Network.Dot4GClient.GetRunnerIdAsync();
+ 
+            if (runnerId.Value == 1)
+            {
+                var runnerState = await Network.Dot4GClient.GetRunnerStateAsync(runnerId);
+                if (runnerState != null && runnerState.Value == RunnerState.Accepted)
+                {
+                    return SearchState.MatchFound;
+                } 
+
+                return SearchState.CheckRunnerState;
+            }
+            else if (playerQueued.Value == 0)
+            {
+                return SearchState.NeedToQueue;
+            }
+            else if (playerQueued.Value == 1)
+            {
+                return SearchState.WaitForPlayers;
+            }
+
+            return SearchState.None;
         }
 
         IEnumerator MatchFound()
