b9a35bff360c07ab1b4f0380572823ebd538f8a7
cedric.decoster <cedric.decoster@gmail.com>
2022-12-08 13:23:23 +0100

    refactoring to package building structure

---
+++ Assets/Ajuna Network/DOT4G/Scripts/Game/Timer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : UICanvas
+{
+    [SerializeField]private Slider timerSlider;
+    private bool updateUI = true;
+    
+    public void StartTimer()
+    {
+        InvokeRepeating("TimerTick", 0, 1f);
+    }
+
+    public void ResetTimer()
+    {
+        timerSlider.value = 30;
+    }
+
+    public void StopTimer()
+    {
+        CancelInvoke("TimerTick");
+    }
+        
+     void TimerTick()
+    {
+        if (updateUI)
+        {
+            timerSlider.value--; 
+        }
+    }
+     
+     //tempfunctin to force the timer to end
+     public void ForceStopTimer()
+     {
+         updateUI = false;
+         
+         CancelInvoke("TimerTick");
+         InvokeRepeating("TimerTick", 0, 30f);
+     }
+}
